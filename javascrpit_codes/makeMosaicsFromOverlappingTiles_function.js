7e0986e0586dec0f5f71297d30d48bbefd87625d
Daniel Paluba <33784015+palubad@users.noreply.github.com>
2023-02-14 12:39:22 +0100

    Update makeMosaicsFromOverlappingTiles_function.js
    fixed function

--- javascrpit_codes/makeMosaicsFromOverlappingTiles_function.js
+++ javascrpit_codes/makeMosaicsFromOverlappingTiles_function.js
@@ -7,14 +7,14 @@ var makeMosaicsFromOverlappingTiles = function (collection, ROI) {
     var date = ee.Date(thisImage.get('system:time_start'));
     // add 1 hour to exclude images from other paths
     var filteredDataset = collection.filterBounds(ROI)
-                          .filterDate(date, date.advance(1,'hour'));
+                          .filterDate(date.advance(-1,'hour'), date.advance(1,'hour'));
     // add all image properties to the new image
     var toReturn = ee.Image(filteredDataset.mosaic()
                       .copyProperties(image,image.propertyNames())
-                      );
+                      ).set('timeD',ee.Date(thisImage.get('system:time_start')).format('YYYY-MM-dd-HH'));
     // add geometries
     var geometries = filteredDataset.map(function(img){
-      return ee.Feature(img.geometry());
+      return ee.Geometry(img.geometry());
     });
     
     var mergedGeometries = geometries.union();
@@ -24,8 +24,8 @@ var makeMosaicsFromOverlappingTiles = function (collection, ROI) {
   var mosaiced = collection.map(makeMosaics);
   
   // the final collection without duplicates
-  var post_filtered = mosaiced.filter(ee.Filter.contains('.geo', ROI));
-  return post_filtered;
+  var post_filtered = mosaiced.distinct(['.geo','timeD']);
+  return ee.ImageCollection(post_filtered);
   
 }
 
